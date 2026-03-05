4878c6c9d7f318c76b87891c6e3343a601390f21
Lucas Moraes <kkoozzmmoo@gmail.com>
2026-03-05 14:38:27 -0300

    Aula spinner

--- app/src/main/java/com/example/projetocandidaturas/CandidaturaActivity.java
+++ app/src/main/java/com/example/projetocandidaturas/CandidaturaActivity.java
@@ -2,28 +2,34 @@ package com.example.projetocandidaturas;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class CandidaturaActivity extends AppCompatActivity {
 
     private EditText editTextNome, editTextEmpresa;
     private CheckBox checkBoxIndicacao;
     private RadioGroup radioGroupReg;
+    private Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_candidatura);
 
-        editTextNome = findViewById(R.id.editTextNome);
-        editTextEmpresa = findViewById(R.id.editTextEmpresa);
+        editTextNome      = findViewById(R.id.editTextNome);
+        editTextEmpresa   = findViewById(R.id.editTextEmpresa);
         checkBoxIndicacao = findViewById(R.id.checkBoxIndicacao);
-        radioGroupReg = findViewById(R.id.radioGroupRegime);
+        radioGroupReg     = findViewById(R.id.radioGroupRegime);
+        spinner           = findViewById(R.id.spinnerFaixaSalarial);
     }
 
     public void limparCampos(View view) {
@@ -31,6 +37,7 @@ public class CandidaturaActivity extends AppCompatActivity {
         editTextEmpresa.setText(null);
         checkBoxIndicacao.setChecked(false);
         radioGroupReg.clearCheck();
+        spinner.setSelection(0);
 
         editTextNome.requestFocus();
 
@@ -39,7 +46,7 @@ public class CandidaturaActivity extends AppCompatActivity {
 
     public void salvarValores(View view) {
 
-        var nome = editTextNome.getText().toString();
+        var nome    = editTextNome.getText().toString();
         var empresa = editTextEmpresa.getText().toString();
 
         if (nome == null || nome.isBlank()) {
@@ -65,12 +72,21 @@ public class CandidaturaActivity extends AppCompatActivity {
             regime = getString(R.string.pessoaF);
         } else {
             Toast.makeText(this, R.string.favor_selecionar_regime, Toast.LENGTH_LONG).show();
+            return;
         }
+
+        if (spinner.getSelectedItem() == null) {
+            Toast.makeText(this, "Spinner sem valores.", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        var faixa = spinner.getSelectedItem().toString();
 
         Toast.makeText(this,
                 getString(R.string.nome_valor) + nome + "\n"
                         + getString(R.string.empresa_valor) + empresa + "\n"
                         + getString(R.string.indicacao_valor) + isIndicacao + "\n"
-                        + getString(R.string.regime_contratacao_valor) + regime, Toast.LENGTH_LONG).show();
+                        + getString(R.string.regime_contratacao_valor) + regime + "\n"
+                        + "Faixa Selecionada: " + faixa, Toast.LENGTH_LONG).show();
     }
 }
